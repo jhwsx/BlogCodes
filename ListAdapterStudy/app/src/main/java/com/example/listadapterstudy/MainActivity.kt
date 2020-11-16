084bc239d8d4b01ad1dbc5826925e9f4652b9140
wangzhichao <willwaywang6@gmail.com>
2020-11-16 19:06:20 +0800

    ListAdapterStudy

--- ListAdapterStudy/app/src/main/java/com/example/listadapterstudy/MainActivity.kt
+++ ListAdapterStudy/app/src/main/java/com/example/listadapterstudy/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupAdapter()
+        setupSmartRefreshLayout()
     }
 
     private fun setupAdapter() {
@@ -21,14 +22,32 @@ class MainActivity : AppCompatActivity() {
         binding.recyclerView.layoutManager = GridLayoutManager(this, 2)
         binding.recyclerView.addItemDecoration(GridSpaceDecoration.newInstance(10))
         binding.recyclerView.adapter = adapter
-        adapter.submitList(getList())
+        adapter.submitList(getPageList())
     }
 
+    private fun setupSmartRefreshLayout() {
+        binding.refreshLayout.setEnableRefresh(false)
+        binding.refreshLayout.setOnLoadMoreListener {
+            lastIndex = lastIndex + pageNumber
+            if (lastIndex >= 50) {
+                binding.refreshLayout.finishRefreshWithNoMoreData()
+                return@setOnLoadMoreListener
+            }
+            val pageList = getPageList()
+            val newList = mutableListOf<Item>()
+            newList.addAll(adapter.currentList)
+            newList.addAll(pageList)
+            adapter.submitList(newList)
+            binding.refreshLayout.finishLoadMore(300)
+        }
+    }
 
-    private fun getList(): List<Item> {
+    private var lastIndex: Int = 1
+    private var pageNumber = 10
+    private fun getPageList(): List<Item> {
         val colorList = getColorList()
         var result = mutableListOf<Item>()
-        for (i in 1..50) {
+        for (i in lastIndex until lastIndex + pageNumber) {
             result.add(Item(i, colorList[i % colorList.size]))
         }
         return result
